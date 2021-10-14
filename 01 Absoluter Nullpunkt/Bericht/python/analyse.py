7b9ceeeb4c05e59e80a39a91b09bc2197725519e
gosow9 <cedric.r.sbx@swissonline.ch>
2021-10-14 15:19:43 +0200

    analyse

--- 01 Absoluter Nullpunkt/Bericht/python/analyse.py
+++ 01 Absoluter Nullpunkt/Bericht/python/analyse.py
@@ -7,6 +7,14 @@ Created on Mon Oct 11 16:29:18 2021
 import numpy as np
 from scipy import stats as stats
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
 from scipy.optimize import leastsq, fsolve
 import scipy.optimize as optimization
 from scipy.stats import t
@@ -26,7 +34,7 @@ p_luft = 95711.7612
 p_vakuum = 10
 
 # Gemessener druck in milivolt
-us = np.linspace(-10, 140, 1000)
+us = np.linspace(-10, 150, 1000)
 u_luft = 132.21
 u_vakuum = -3.15
 u_Wkoch = 131.28
@@ -107,7 +115,28 @@ def p_from_u(U, c, p0):
     """
     return p0 + c*U
 
+error = 0.075
+plt.figure()
+yti1 = [-20000.,0.,4000,20000.,40000.,60000.,80000.,100000.]
+yti2 = [-20000.,0.,2237.1,20000.,40000.,60000.,80000.,100000.]
+plt.yticks(yti1,yti2)
+#plt.fill_between(us, p_from_u(us-error, c1, p1), p_from_u(us+error, c1, p1))
+plt.plot(u_luft, p_luft, 'o', label= r'Ambient pressure')
+plt.plot(u_vakuum, p_vakuum, 'o', label= r'Vacuum')
+plt.plot(us, p_from_u(us, c1, p1),linestyle='--', linewidth=1, label= r'Calculated slope $c$')
+plt.plot(0, p1, 'o', label= r'Calculated offset $p0$')
+print(p1)
+#plt.vlines(x=0, ymin=-15000, ymax=p1, linewidth=1, color='k', linestyle='--')
+#plt.hlines(y=p1, xmin=-20, xmax=0, linewidth=1, color='k', linestyle='--')
+plt.xlim(left=-10, right = 150)
+plt.ylim(bottom = -1000, top = 100000)
+plt.ylabel(r'Pressure in pa')
+plt.xlabel(r'Pressure in mV')
+plt.legend()
 
-plt.plot(us, p_from_u(us, c1, p1))
-plt.plot(u_luft, p_luft, 'o')
-plt.plot(u_vakuum, p_vakuum, 'o')
+
+#plt.xticks(list(plt.xticks()[0]) + extraticks)
+#plt.grid(True)
+plt.savefig("cali.pgf")
+#tikzplotlib.clean_figure()
+#tikzplotlib.save("cali.tex")
